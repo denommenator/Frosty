6952646faf539ba601821c61b0f708cc65673d27
robert-denomme <robert.a.denomme@gmail.com>
2024-09-24 14:03:57 -0400

    GPU instancing with colors!

---
+++ Assets/ParticleShader.shader
@@ -0,0 +1,53 @@
+Shader "Unlit/ParticleShader"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_instancing
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            StructuredBuffer<float4> _Colors;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_TRANSFER_INSTANCE_ID(v, o);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                UNITY_SETUP_INSTANCE_ID(i);
+                int instance_id = 0;
+                #ifdef UNITY_INSTANCING_ENABLED
+                    instance_id = unity_InstanceID;
+                #endif
+                
+                return _Colors[instance_id];
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/Renderers.cs
+++ Assets/Renderers.cs
@@ -9,21 +9,50 @@ public class BallRenderer
     public GameObject[] Balls;
     public List<Particle> Particles;
     public List<Matrix4x4[]> BatchMatrices;
+    public List<ComputeBuffer> BatchColors;
     public Mesh ParticleMesh;
-    public Material ParticleMaterial;
+    public List<Material> ParticleMaterials;
     public BallRenderer(List<Particle> particles)
     {
         BatchMatrices = new List<Matrix4x4[]>();
         int divisor = particles.Count / 1000;
         int remainder = particles.Count - 1000 * divisor;
+        List<float[]> colorData = new List<float[]>();
+        BatchColors = new List<ComputeBuffer>();
+        ParticleMaterials = new List<Material>();
+        Material material = Resources.Load<Material>("Material/ParticleMaterial");
         for (int i = 0; i < divisor; i++)
         {
             BatchMatrices.Add(new Matrix4x4[1000]);
+            colorData.Add(new float[1000 * 4]);
+            BatchColors.Add(new ComputeBuffer(1000, 4 * sizeof(float)));
+            ParticleMaterials.Add(new Material(material));
         }
 
         if (remainder > 0)
         {
             BatchMatrices.Add(new Matrix4x4[remainder]);
+            colorData.Add(new float[remainder * 4]);
+            BatchColors.Add(new ComputeBuffer(remainder, 4 * sizeof(float)));
+            ParticleMaterials.Add(new Material(material));
+        }
+        
+        for (int i = 0; i < particles.Count; ++i)
+        {
+            int divisor_i = i / 1000;
+            int remainder_i = i - 1000 * divisor_i;
+            
+            colorData[divisor_i][4 * remainder_i + 0] = particles[i].Color.r;
+            colorData[divisor_i][4 * remainder_i + 1] = particles[i].Color.g;
+            colorData[divisor_i][4 * remainder_i + 2] = particles[i].Color.b;
+            colorData[divisor_i][4 * remainder_i + 3] = particles[i].Color.a;
+        }
+
+        for (int i = 0; i < colorData.Count; i++)
+        {
+            BatchColors[i].SetData(colorData[i]);
+            ParticleMaterials[i].enableInstancing = true;
+            
         }
 
         Particles = particles;
@@ -31,8 +60,9 @@ public class BallRenderer
         GameObject particle = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         ParticleMesh = particle.GetComponent<MeshFilter>().mesh;
         GameObject.Destroy(particle);
-        ParticleMaterial = new Material(Shader.Find("Standard"));//particle.GetComponent<MeshRenderer>().material;
-        ParticleMaterial.enableInstancing = true;
+        //ParticleMaterial = Resources.Load<Material>("Material/ParticleMaterial"); //new Material(Shader.Find("Standard"));//particle.GetComponent<MeshRenderer>().material;
+        //ParticleMaterial.enableInstancing = true;
+        
         /*
         Balls = new GameObject[particles.Count];
         //DeformationLines = new GameObject[numParticles];
@@ -60,12 +90,14 @@ public class BallRenderer
             BatchMatrices[divisor][remainder] = Matrix4x4.TRS(particlePositions[i], Quaternion.identity, ballShrinkage * Particles[i].Radius * Vector3.one);
         }
 
-        foreach (Matrix4x4[] Matrices in BatchMatrices)
+        for(int batch = 0; batch < BatchMatrices.Count; ++batch)
         {
+            Matrix4x4[] Matrices = BatchMatrices[batch];
+            ParticleMaterials[batch].SetBuffer("_Colors", BatchColors[batch]);
             Graphics.DrawMeshInstanced(
                 ParticleMesh,
                 0,
-                ParticleMaterial,
+                ParticleMaterials[batch],
                 Matrices
             );
         }
